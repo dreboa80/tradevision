5af6cd66da72c87c34d005cf537e2fcf8e2a485c
dreboa80 <marcel@moneyspaces.xyz>
2025-12-25 20:10:43 +0300

    Delete api directory

--- api/ping.ts
+++
@@ -1,6 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.setHeader("Content-Type", "application/json; charset=utf-8");
-  res.status(200).json({ ok: true, ping: "pong" });
-}
