d787cc2b944d85b76ce3cb09ecbe090af99a9fa2
dreboa80 <marcel@moneyspaces.xyz>
2025-12-25 20:06:06 +0300

    Create ping.ts

---
+++ api/ping.ts
@@ -0,0 +1,6 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  res.status(200).json({ ok: true, ping: "pong" });
+}
